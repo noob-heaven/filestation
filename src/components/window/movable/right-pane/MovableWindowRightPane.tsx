77995d627a7f72392cf679b101102ce16aca10bb
shellcodesniper <shellcodesniper@icloud.com>
2024-06-30 19:18:05 +0900

    feat: Right-Pane ToolBar

--- src/components/window/movable/right-pane/MovableWindowRightPane.tsx
+++ src/components/window/movable/right-pane/MovableWindowRightPane.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-import SimpleDefaultButton from '@components/button/simple/SimpleDefaultButton';
+import MemoizedSimpleDefaultButton from '@components/button/simple/SimpleDefaultButton';
 
 import styles from './MovableWindowRight.module.css';
 
 export default function MovableWindowRightPane() {
+  const onClickWhat = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('what');
+  }
   return (
-    <div className={styles.rightPaneStyle}>
-      <div className={styles.topBarStyle}>
-        <SimpleDefaultButton>TEXT</SimpleDefaultButton>
-        <SimpleDefaultButton>TEXT</SimpleDefaultButton>
+    <div className={styles.rightPane}>
+      <div className={styles.topBar}>
+        <div className={styles.topBarLeftGroup}>
+          <MemoizedSimpleDefaultButton onClick={onClickWhat}>모</MemoizedSimpleDefaultButton>
+          <MemoizedSimpleDefaultButton>되는건</MemoizedSimpleDefaultButton>
+          <MemoizedSimpleDefaultButton>없지만</MemoizedSimpleDefaultButton>
+        </div>
+        <div className={styles.topBarRightGroup}>
+          <MemoizedSimpleDefaultButton>예쁘게</MemoizedSimpleDefaultButton>
+          <MemoizedSimpleDefaultButton>보이려</MemoizedSimpleDefaultButton>
+        </div>
       </div>
     </div>
   )
