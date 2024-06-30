f9752345170d5fc74d936e596381151c559572aa
shellcodesniper <shellcodesniper@icloud.com>
2024-06-30 19:56:29 +0900

    chore(re-struct): rename all style.module.css

--- src/components/file/tree/leaf/NonTerminalLeaf.tsx
+++ src/components/file/tree/leaf/NonTerminalLeaf.tsx
@@ -7,7 +7,7 @@ import { ITreeData } from '@components/file/@types/tree.data';
 import ConditionalNonTerminalSelector from './ConditionalNonTerminalSelector';
 
 import MemoizedSpacer from '../utillity/Spacer';
-import styles from '../tree.module.css';
+import styles from './style.module.css';
 
 // NOTE: 논터미널 -> 단말 노드를 가지고 있는것들임 <li>해당 리프 내용물 inside: <ul>{내용물}</ul> </li> 로 해줘야함
 export function NonTerminalLeaf(props: ITreeData) {
