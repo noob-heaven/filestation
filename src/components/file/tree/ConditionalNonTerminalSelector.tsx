2aa040a10322bda9369fc6d8bebc7cf1407fd5cb
shellcodesniper <shellcodesniper@icloud.com>
2024-06-30 18:28:42 +0900

    feat: Hashed key

--- src/components/file/tree/ConditionalNonTerminalSelector.tsx
+++ src/components/file/tree/ConditionalNonTerminalSelector.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { simpleHash } from '@common/utils/hash.utils';
+
 import { ITreeData } from './@types/tree.data';
 import NonTerminalLeaf from './NonTerminalLeaf';
 import TerminalLeaf from './TerminalLeaf';
@@ -10,11 +13,14 @@ export default function ConditionalNonTerminalSelector({ datas }: { datas: ITree
     <>
       {
         // children 돌면서, isLeaf -> Terminal, !isLeaf -> NonTerminal 로
-        (datas || []).map((val, valIdx) => (
-          (!val.isLeaf) // NOTE: 가지일경우임!
-            ? <NonTerminalLeaf {...val} key={val.path} />
-            : <TerminalLeaf {...val} key={val.path} />
-        ))
+        (datas || []).map((val) => {
+          const keyHash = simpleHash(val.path);
+          return (
+            (!val.isLeaf) // NOTE: 가지일경우임!
+              ? <NonTerminalLeaf {...val} key={keyHash} />
+              : <TerminalLeaf {...val} key={keyHash} />
+          );
+        })
       }
     </>
   );
