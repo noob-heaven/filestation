d53f28b024f263931f39efb68a9253f8876d473d
shellcodesniper <shellcodesniper@icloud.com>
2024-06-30 21:28:42 +0900

    feat: 폴더 목록 & Overflow 자동으로 가려주기

--- src/components/window/movable/right-pane/body/BodyContainer.tsx
+++ src/components/window/movable/right-pane/body/BodyContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import BrowserPane from '@components/file/browser/pane/BrowserPane';
+
 import styles from './style.module.css';
 
 export default function BodyContainer() {
   return (
     <div className={styles.bodyContainer}>
-      <h1>Body</h1>
+      {/* 보기 목록(목록 / 아이콘)에 따라 조건부 랜더링을 이쪽에서 구성 */}
+      <BrowserPane />
     </div>
   );
 }
